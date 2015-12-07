3ad56f25cd95e25125dc44af63050a2ecd20111e
Jason DeTiberus <jdetiber@redhat.com>
2015-12-06 21:53:56 -0500

    start for kubernetes pod module

---
+++ cloud/kubernetes/kubernetes_pod.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# coding: utf-8 -*-
+
+# Copyright (c) 2015 Red Hat, Inc
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+
+DOCUMENTATION = '''
+---
+module: kubernetes_pod
+short_description: Create, Delete or Modify a Kubernetes Pod
+version_added: "2.0"
+author: "Jason DeTiberus <jdetiber@redhat.com>"
+description:
+   - Create, Delete or Modify a Kubernetes Pod
+options:
+   TODO
+requirements:
+   TODO
+'''
+
+EXAMPLES = '''
+TODO
+'''
+
+def main():
+    argument_spec = kubernetes_argument_spec(
+        name                            = dict(required=True),
+        state                           = dict(default='present', choices=['absent', 'present']),
+    )
+    module_kwargs = openstack_module_kwargs(
+        mutually_exclusive=[],
+        required_together=[],
+        required_one_of=[]
+    )
+    module = AnsibleModule(argument_spec, **module_kwargs)
+
+    kube_client = KubernetesClient(module)
+
+    state = module.params['state']
+    pod_name = module.params['name']
+
+    pod = kube_client.get_pod(pod_name)
+
+    if state == 'present':
+        if pod is not None:
+            kube_client.update_pod(pod_name)
+        else:
+            kube_client.create_pod(pod_name)
+    elif state == 'absent':
+        if pod is not None:
+            kube_client.delete_pod(pod_name)
+
+# this is magic, see lib/ansible/module_common.py
+from ansible.module_utils.basic import *
+from ansible.module_utils.urls import *
+from ansible.module_utils.kubernetes import *
+if __name__ == '__main__':
+    main()
